107af55a409818781bb8ef5c119bc8b73619e609
zhangxian <1756569043@qq.com>
2021-04-13 15:46:34 +0800

    bug fix

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # cnn
 nohup python -u train_gait_cnn.py --img_type time_only_two_channel --epoch 60 --cuda 0 > time_only_two_channel.log 2>&1 &
-nohup python -u train_gait_cnn.py --img_type counts_only_two_channel --epoch 60 --cuda 0 > counts_only_two_channel.log 2>&1 &
-nohup python -u train_gait_cnn.py --img_type counts_and_time_two_channel --epoch 60 --cuda 1 > counts_and_time_two_channel.log 2>&1 &
+nohup python -u train_gait_cnn.py --img_type counts_only_two_channel --epoch 60 --cuda 1 > counts_only_two_channel.log 2>&1 &
+nohup python -u train_gait_cnn.py --img_type counts_and_time_two_channel --epoch 60 --cuda 0 > counts_and_time_two_channel.log 2>&1 &
 nohup python -u train_gait_cnn.py --img_type four_channel --epoch 60 --cuda 1 > four_channel.log 2>&1 &
 
 # gcn
